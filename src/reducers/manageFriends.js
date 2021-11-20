50df50521c0d3cbc88fb797b15bc8f8b7e4a2864
ryliespriggs <rspriggs812@gmail.com>
2021-11-20 12:49:40 -0800

    commit tree a7f1b727958eaaf9f749d0a3b6acb987cc4e504c

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [],
+}, action){
+    switch(action.type){
+        case 'ADD_FRIEND':
+            return (
+                {...state,
+                friends: [
+                    ...state.friends,
+                    action.friend
+                ]}
+            )
+        case 'REMOVE_FRIEND':
+            const deleteFren = state.friends.findIndex( friend => friend.id === action.id)
+            return (
+                {...state,
+                    friends: [
+                        ...state.friends.slice(0, deleteFren),
+                        ...state.friends.slice(deleteFren + 1)
+                        ]}
+                    )
+        default:
+            return state
+    }
 }
